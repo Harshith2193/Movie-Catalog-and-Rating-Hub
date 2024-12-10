4beeb91844d5e00f943028fc4237e465e000c95d
Harshith T <157876125+Harshith2193@users.noreply.github.com>
2024-12-10 16:45:32 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+
+    $name= $_POST['name']
+    $email= $_POST['email']
+    $dob= $_POST['dob']
+    $username= $_POST['username']
+    $password= $_POST['password']
+
+
+    //connection
+    $conn =new mysqli('localhost','root','','trial');
+    if($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+    }else{
+        $stmt =$conn -> prepare("insert into signup(name, email, dob, username, password) values(?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $name, $email, $dob, $username, $password);
+        $stmt->execute();
+        echo "registration successful...";
+        $stmt->close();
+        $conn->close();
+    }
+?>
